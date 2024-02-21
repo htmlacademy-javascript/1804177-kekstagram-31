1d9417b67921b1fdb0734ca6f5342c2173499839
Yakov <zif1406@gmail.com>
2024-02-21 12:07:46 +0700

    additional task

--- js/functions.js
+++ js/functions.js
@@ -1,8 +1,8 @@
 const stringLength = (string, maxLength) => string.length <= maxLength;
 
-stringLength('проверяемая строка', 20);
-stringLength('проверяемая строка', 18);
-stringLength('проверяемая строка', 10);
+(stringLength('проверяемая строка', 20));
+(stringLength('проверяемая строка', 18));
+(stringLength('проверяемая строка', 10));
 
 const palindrome = (string) => {
   const newString = string.replaceAll(' ', '').toLowerCase();
@@ -17,3 +17,19 @@ const palindrome = (string) => {
 palindrome('топот');
 palindrome('ДовОд');
 palindrome('Кекс');
+
+const extractNumbers = (string = '') => {
+  let result = '';
+  for (let i = 0; i <= string.length - 1; i++) {
+    if (!Number.isNaN(parseInt(string[i], 10))) {
+      result += string[i];
+    }
+  }
+  return result === '' ? NaN : Number(result);
+};
+
+extractNumbers('2023 год');
+extractNumbers('ECMAScript 2022');
+extractNumbers('1 кефир, 0.5 батона');
+extractNumbers('агент 007');
+extractNumbers('а я томат');
