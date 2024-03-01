8965bef654fad8a4d11380d10eafade619eb2a72
Yakov <zif1406@gmail.com>
2024-03-01 19:27:45 +0700

    Additional homework

--- js/functions.js
+++ js/functions.js
@@ -33,3 +33,18 @@ extractNumbers('ECMAScript 2022');
 extractNumbers('1 кефир, 0.5 батона');
 extractNumbers('агент 007');
 extractNumbers('а я томат');
+
+const workTime = (elements) => {
+  const timeArr = elements.split(':');
+  const [hourse, minutes] = timeArr;
+  return Number(hourse) * 60 + Number(minutes);
+};
+const isMeetingWithinWorkday = (startOfWorkday, endOfWorkday, meetingStart, meetingDuration) => {
+  const TotalMeetingTime = workTime(meetingStart) + meetingDuration;
+  return workTime(startOfWorkday) < TotalMeetingTime && workTime(endOfWorkday) >= TotalMeetingTime;
+};
+isMeetingWithinWorkday('8:0', '10:0', '8:0', 120);
+isMeetingWithinWorkday('8:0', '10:0', '8:0', 120);
+isMeetingWithinWorkday('08:00', '14:30', '14:00', 90);
+isMeetingWithinWorkday('14:00', '17:30', '08:0', 90);
+isMeetingWithinWorkday('8:00', '17:30', '08:00', 900);
