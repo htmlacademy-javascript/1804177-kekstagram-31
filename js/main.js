d0bb0c9ec36d06a2282ca72b3cb36b1637a18ee2
Yakov <zif1406@gmail.com>
2024-03-25 18:30:52 +0700

    Added form validation

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@ import './util.js';
 import {getUserData} from './data.js';
 import {renderPictures} from './rendering.js';
 import {modalClickHandler} from './modal/modal-click-handler.js';
+import './form/form.js';
+import {uploadFiles} from './form/form-click-handler.js';
 
 renderPictures(getUserData());
 modalClickHandler();
+uploadFiles();
